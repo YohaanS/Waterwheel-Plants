39cd2747ea6f1c30ca16e9acc411834acafb7f73
YohaanS <yohaan.singla@gmail.com>
2024-10-05 18:08:13 -0700

    New folder name

--- infoStart/asgi.py
+++ infoStart/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for WaterwheelPlant project.
+ASGI config for infoStart project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WaterwheelPlant.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infoStart.settings')
 
 application = get_asgi_application()
--- infoStart/wsgi.py
+++ infoStart/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for WaterwheelPlant project.
+WSGI config for infoStart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WaterwheelPlant.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infoStart.settings')
 
 application = get_wsgi_application()
